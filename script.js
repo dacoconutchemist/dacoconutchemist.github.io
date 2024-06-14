c5872e7e65fdd1a1f561fa0ef39b9050985e24f7
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2024-06-14 18:41:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var pages = {
-	"Price Calculator": ["/pricecalc", "calculated the minimal allowed prices for goods on GummerCraft"],
+    "Price Calculator": ["/pricecalc", "calculated the minimal allowed prices for goods on GummerCraft"],
     "Translator": ["/translate", "translates fictional languages on GummerCraft, doubling as an Ukrainian-to-English keyboard layout fixer"],
     "fx-991DEX typer": ["/fx-991DEX", "converts text to button presses on the fx-991DEX calculator"],
     "Hexguessr": ["/hexguessr", "hone your hex color reading and writing skills!"],
@@ -10,97 +10,96 @@ var pages = {
     "Tampermonkey": ["/tampermonkey", "my userscripts (not available yet)"]
 };
 
-let hashdict = {
-	"#0": "/pricecalc",
-	"#1": "/translate",
-	"#2": "/fx-991DEX",
-	"#3": "/hexguessr",
-	"#4": "/rockpaperscissors",
-	"#5": "/particles",
-	"#6": "/4our4ours",
-	"#7": "/desmos",
-	"#8": "/tampermonkey"
+var hashdict = {
+    "#0": "/pricecalc",
+    "#1": "/translate",
+    "#2": "/fx-991DEX",
+    "#3": "/hexguessr",
+    "#4": "/rockpaperscissors",
+    "#5": "/particles",
+    "#6": "/4our4ours",
+    "#7": "/desmos",
+    "#8": "/tampermonkey"
 }
 
 
 
 
 
-
-
-for (let i in pages) {
-    $('#content').append(`
-    	<div class="col-12 col-sm-6 col-md-6 col-lg-3 tile">
-    		<img src="homepage_images${pages[i][0]}.png" class="image">
-			<h4 style="margin-top: 5px;"><b><a href="${pages[i][0]}">${i}</a></b></h2>
-			${pages[i][1]}
-    	</div>
-    `);
-}
-
-setInterval(() => {
-	document.documentElement.style.height = document.body.getBoundingClientRect().height + "px";	
-}, 50);
-
-var touchscreen = window.DetectIt.primaryInput === 'touch';
-$(window).on("load", () => {
-	let sharebutton = $('#sharebutton');
-	let sharelabel = $('#urlsharelabel');
-	let offsetY = sharebutton[0].getBoundingClientRect().top - sharelabel[0].getBoundingClientRect().top;
-	let defaultTransition = `transform 0.3s ease-out, clip-path 0.3s ease-out`;
-	let touchTimeout1 = undefined, touchTimeout2 = undefined;
-	//console.log(offsetY);
-	let mouseEnterEvent = () => {
-		sharelabel.css("transform", `translateY(0px)`);
-		sharelabel.css("clip-path", `polygon(0 0, 100% 0, 100% 120%, 0 120%)`);
-		sharelabel.text("da.gd/ab");
-	};
-	let mouseLeaveEvent = () => {
-		sharelabel.css("transform", `translateY(${offsetY}px)`);
-		sharelabel.css("clip-path", `polygon(0 0, 100% 0, 100% 0, 0 0)`);
-	};
-	if (touchscreen) {
-		mouseEnterEvent();
-		sharelabel[0].offsetHeight; // https://stackoverflow.com/a/16575811
-		sharelabel.css("opacity", `1`);
-		sharelabel.css("transition", defaultTransition);
-	} else {
-		mouseLeaveEvent();
-		sharelabel[0].offsetHeight; // https://stackoverflow.com/a/16575811
-		sharelabel.css("opacity", `1`);
-		sharelabel.css("transition", defaultTransition);
-		sharebutton.hover(mouseEnterEvent, mouseLeaveEvent);
-	}
-	sharebutton.on("click", () => { 
-	    navigator.clipboard.writeText("da.gd/ab").then(
-	    	() => {
-	    		mouseEnterEvent();
-	    		clearTimeout(touchTimeout1);
-	    		clearTimeout(touchTimeout2);
-	    		sharelabel.css("transition", defaultTransition);
-	    		sharelabel.css("color", `#00cc00`);
-	    		sharelabel[0].offsetHeight; // https://stackoverflow.com/a/16575811
-	    		sharelabel.css("transition", `${defaultTransition}, color 1s ease-out`);
-	    		sharelabel.css("color", `#ffffff`);
-	    		sharelabel.text("Copied!");
-	    		if (touchscreen) {
-	    			touchTimeout1 = setTimeout(() => {
-	    				mouseLeaveEvent();
-	    				touchTimeout2 = setTimeout(() => {
-		    				mouseEnterEvent();
-		    			}, 300);
-	    			}, 1000);
-	    		}
-	    	},
-	    	() => alert('Copying failed')
-	    );
-	});
-});
 
 if (document.location.hash in hashdict) {
-	document.location.replace(
-		"https://dacoconutchemist.github.io" + 
-		hashdict[document.location.hash] +
-		document.location.search.toString()
-	);
+    document.location.replace(
+        "https://dacoconutchemist.github.io" + 
+        hashdict[document.location.hash] +
+        document.location.search.toString()
+    );
+} else {
+    for (let i in pages) {
+        $('#content').append(`
+            <div class="col-12 col-sm-6 col-md-6 col-lg-3 tile">
+                <img src="homepage_images${pages[i][0]}.png" class="image">
+                <h4 style="margin-top: 5px;"><b><a href="${pages[i][0]}">${i}</a></b></h2>
+                ${pages[i][1]}
+            </div>
+        `);
+    }
+
+    setInterval(() => {
+        document.documentElement.style.height = document.body.getBoundingClientRect().height + "px";    
+    }, 50);
+
+    var touchscreen = window.DetectIt.primaryInput === 'touch';
+    $(window).on("load", () => {
+        let sharebutton = $('#sharebutton');
+        let sharelabel = $('#urlsharelabel');
+        let offsetY = sharebutton[0].getBoundingClientRect().top - sharelabel[0].getBoundingClientRect().top;
+        let defaultTransition = `transform 0.3s ease-out, clip-path 0.3s ease-out`;
+        let touchTimeout1 = undefined, touchTimeout2 = undefined;
+        //console.log(offsetY);
+        let mouseEnterEvent = () => {
+            sharelabel.css("transform", `translateY(0px)`);
+            sharelabel.css("clip-path", `polygon(0 0, 100% 0, 100% 120%, 0 120%)`);
+            sharelabel.text("da.gd/ab");
+        };
+        let mouseLeaveEvent = () => {
+            sharelabel.css("transform", `translateY(${offsetY}px)`);
+            sharelabel.css("clip-path", `polygon(0 0, 100% 0, 100% 0, 0 0)`);
+        };
+        if (touchscreen) {
+            mouseEnterEvent();
+            sharelabel[0].offsetHeight; // https://stackoverflow.com/a/16575811
+            sharelabel.css("opacity", `1`);
+            sharelabel.css("transition", defaultTransition);
+        } else {
+            mouseLeaveEvent();
+            sharelabel[0].offsetHeight; // https://stackoverflow.com/a/16575811
+            sharelabel.css("opacity", `1`);
+            sharelabel.css("transition", defaultTransition);
+            sharebutton.hover(mouseEnterEvent, mouseLeaveEvent);
+        }
+        sharebutton.on("click", () => { 
+            navigator.clipboard.writeText("da.gd/ab").then(
+                () => {
+                    mouseEnterEvent();
+                    clearTimeout(touchTimeout1);
+                    clearTimeout(touchTimeout2);
+                    sharelabel.css("transition", defaultTransition);
+                    sharelabel.css("color", `#00cc00`);
+                    sharelabel[0].offsetHeight; // https://stackoverflow.com/a/16575811
+                    sharelabel.css("transition", `${defaultTransition}, color 1s ease-out`);
+                    sharelabel.css("color", `#ffffff`);
+                    sharelabel.text("Copied!");
+                    if (touchscreen) {
+                        touchTimeout1 = setTimeout(() => {
+                            mouseLeaveEvent();
+                            touchTimeout2 = setTimeout(() => {
+                                mouseEnterEvent();
+                            }, 300);
+                        }, 1000);
+                    }
+                },
+                () => alert('Copying failed')
+            );
+        });
+    });
 }
