0c1ed6c57f8b9f23efcfa31da93a5a0c713d63e3
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2024-08-19 11:55:01 +0300

    Create mandelbrotV1.js

---
+++ codegolf/mandelbrotV1.js
@@ -0,0 +1,24 @@
+// paste this into the console in Chrome and wait
+
+                                    M
+                                 =''
+ /* by dacoconutchemist */      ;for(
+                                 n=0;
+                          n< 1e6;n++){X=
+                          Y=i=0;while(X*X+Y
+                        *Y<=4&i<1e3)[X,Y]=[
+                        X*X-Y*Y+n%1e3*275e-5-
+               17/8,2  *X*Y+(n/1e3|0)*275e-5
+             -11/8],i++;C=String["fromCharC"
+           +"ode"](i-1e3?i%4*64:0);M+=C+C+C
+}console.log("%c   ",`font-size:15cm;back`
+           +`ground:url('data:image/bmp;ba`
+             +`se64,Qk32xi0AAAAAADYAAAAoAAA`
+               +`A6A`  +`MAAOgDAAABABgAAAAA`
+                       +`AMDGLQATCwAAEwsAAAA`
+                        +`AAAAAAAAA${btoa(M
+                          )}') no-repeat t`
+                          +`op left/con`
+                                +`ta`
+                                +`in`
+                                 );;
