4e44c197922dc3f711b36d8b40cbf78f3779e49e
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2024-11-07 20:27:17 +0200

    Create consoleQlockV2.js

---
+++ codegolf/consoleQlockV2.js
@@ -0,0 +1,9 @@
+($=_=>{setInterval(G=>(c=console).log(`($=${[...Date(c.clear(B
+=[])).slice(16,24)].map((C,I)=>{for(J=14,Z=parseInt(1+"urfb4n\
+sv7sufmu9uefuffsi9uvfuuf10g".slice(A=C==":"?30:3*C,A+3),32);J+
+1;Z/=2)B[J+(J--/3|0)*28+I*4]=Z&1}),$})()`.replace(/..\n?/g,e=>
+"\x25c"+e),...B.map(e=>e?"background-color:red":"")),1000)})()
+
+// paste into chrome console
+// remake from scratch of https://aem1k.com/qlock/, but in the console
+// v2 is also shorter than the aem1k qlock
