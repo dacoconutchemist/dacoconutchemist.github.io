9bd32396aa4ca3d4ea8827fd584092022a9d19e9
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2024-11-08 00:53:05 +0200

    Create consoleQlockV3.js

---
+++ codegolf/consoleQlockV3.js
@@ -0,0 +1,10 @@
+($=_=>{setInterval(G=>(c=console).log(`($=${[...Date(c.clear(B
+=[])).substr(16,8)].map((C,I)=>{for(J=14,Z=parseInt(`odr8t3mw\
+7mvji4po27o33mkpohbog${''/*daco qlock*/}fsw`.substr(C>"9"?30:3
+*C,3),36);J+1;Z/=2)B[J+(J--/3|0)*28+I*4]=Z&1&&"background-col\
+or:tan"}),$})()`.replace(/..\n?/g,e=>"\x25c"+e),...B),1e3)})()
+
+// paste into chrome console
+// remake from scratch of https://aem1k.com/qlock/, but in the console
+// v3 with some small ideas "borrowed" from the original
+// change "tan" to "red" on the bottom left for better contrast on light themes
