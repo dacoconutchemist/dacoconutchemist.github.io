130dd76b5cc5dfa47b3135e9d14c694fa836f905
dacoconutchemist <75437100+dacoconutchemist@users.noreply.github.com>
2024-11-19 19:01:09 +0200

    Create donutshadowV1.js

---
+++ codegolf/donutshadowV1.js
@@ -0,0 +1,4 @@
+setInterval(P=>{for(i=680,Q='line-height:99q;text-shadow:0 0';i;Q+=`,#fff${'eca69420'[4+P(c(--i)*p,s(i)*p,o=s(h))[1]*4|0]} ${[B,C]=P((a=8+4*p)*c(i),a*s(i),4*o),B*4|0}q ${C*4|0}q`)p=c(h=i%26);W+=1/9;V+=.1;console.log('%c\t\t⋅\t\t',Q)},W=V=110,(x,y,z)=>[x*c(V)-(w=y*c(W)-z*s(W))*s(V),x*s(V)+w*c(V)],{sin:s,cos:c}=Math)
+
+// paste into browser console. tested on chrome and firefox, no guarantee of working on other browsers
+// this prints a donut to the console without ascii art OR images
